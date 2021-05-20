f441fd33e02a0cb1882cc7fcb4edb98479b55aec
NHebrard <nicolas.hebrard@outlook.com>
2021-05-20 19:40:59 +0200

    Fix default route

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom'
 import './App.css'
 import { Footer } from './components/Footer'
 import Header from './components/header/Header'
@@ -12,8 +12,11 @@ function App() {
       <Router>
         <Header />
         <Switch>
-          <Route path="/" exact component={() => <Home />} />
           <Route path="/faq" exact component={() => <Faq />} />
+          <Route path="/" exact component={() => <Home />} />
+          <Route path="/">
+            <Redirect to="/" />
+          </Route>
         </Switch>
       </Router>
       <Footer />
